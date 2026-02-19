6e62a486868efe2c5173adbf0b51f76e8bd913f0
Arthur Antonelli <arthur.antonelli76@gmail.com>
2026-02-19 20:18:04 -0300

    Aula 04

---
+++ aula-04/OlaMundo.java
@@ -0,0 +1,17 @@
+public class OlaMundo{
+
+    public static void main(String[] args){
+        System.out.println("Ola, mundo!");
+
+        int i = 1;
+        String s;
+        switch (i) {
+            case 1:
+                s = "Um";
+                break;
+            default:
+                s = "Outro";
+                break;
+        }
+    }
+}
