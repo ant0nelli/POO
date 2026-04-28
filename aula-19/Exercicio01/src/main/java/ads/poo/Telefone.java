b7cf6eb22d917b655c74cccf29a48633d5fba2fb
arthur antonelli <arthur.antonelli76@gmail.com>
2026-04-27 22:29:04 -0300

    Exercicio01 - Telefone

--- aula-19/Exercicio01/src/main/java/ads/poo/Telefone.java
+++ aula-19/Exercicio01/src/main/java/ads/poo/Telefone.java
@@ -1,18 +1,45 @@
 package ads.poo;
 
 import javax.swing.text.MaskFormatter;
+
+import java.text.ParseException;
 import java.util.HashMap;
 
 public class Telefone {
-    private HashMap<String,String> dados;
+    private HashMap<String,String> dados = new HashMap<>();
 
-    public boolean add(){
-        String rotulo;
-        String valor;
+    public boolean add(String rotulo, String valor){
+        if ((rotulo.equals("pessoal") || rotulo.equals("comercial")) && verificarTelefone(valor) ){
+            dados.put(rotulo, valor);
+            return true;
+        }else return false;
+    }
+
+    public boolean remove(String rotulo){
+        if (dados.containsKey(rotulo)){
+            dados.remove(rotulo);
+            return true;
+        }else return false;
+    }
+
+
+    public boolean update(String rotulo, String valor){
+        if (dados.containsKey(rotulo)){
+            dados.put(rotulo, valor);
+            return true;
+        }else return false;
 
     }
 
-    private boolean verificarTelefone(int valor){
+
+    private boolean verificarTelefone(String valor){
+        String eR = "^[0-9]+$";
+        if (valor.matches(eR)) return true;
+        else return false;
+        
+    }
+
+    public String toString(){
         MaskFormatter mask = null;
         String resultado = "";
 
@@ -20,9 +47,11 @@ public class Telefone {
             mask = new MaskFormatter("(##) ####-####");
             mask.setValueContainsLiteralCharacters(false);
             mask.setPlaceholderCharacter('-');
-            resultado = mask.valueToString(valor);
+            resultado = mask.valueToString(dados.get(rotulo));
+        } catch (ParseException e){
+            e.printStackTrace();
         }
-
+        return resultado;
     }
 
 }
