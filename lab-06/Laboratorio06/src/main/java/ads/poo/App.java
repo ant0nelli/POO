110f4764193e327eae2af9bd46f8ee65d34bb8cf
arthur <arthur.antonelli76@gmail.com>
2026-05-21 20:18:05 -0300

    Laboratorio06

---
+++ lab-06/Laboratorio06/src/main/java/ads/poo/App.java
@@ -0,0 +1,24 @@
+import edu.princeton.cs.algs4.Draw;
+import java.util.concurrent.TimeUnit;
+public class App{
+    // Não iremos tratar as possíveis exceções de execução do programa, por isso o throws Exception
+    public static void main(String[] args) throws Exception{
+        Draw desenho = new Draw();
+        desenho.setCanvasSize(800, 800);
+        desenho.setXscale(0, 800);
+        desenho.setYscale(0, 800);
+// Toda ação de desenho acontecerá em um buffer secundário
+// e esse buffer só será visto depois que for invocado o método show()
+        desenho.enableDoubleBuffering();
+        for (int i = 1; i <= 10; i++) {
+// limpando a área de desenho
+            desenho.clear();
+// escrevendo o valor de i na coordenada (500,500)
+            desenho.text(500, 500, ""+ i);
+// Trocando o buffer para exibir o que foi escrito
+            desenho.show();
+// Dormindo por 1 segundo
+            TimeUnit.SECONDS.sleep(1);
+        }
+    } // fim do main
+}// fim da classe
---
+++ lab-06/Laboratorio06/src/main/java/ads/poo/README.md
@@ -0,0 +1,32 @@
+# Relógios e cronômetros
+
+```mermaid
+classDiagram
+    class Relogio{
+        <<abstract>>
+        
+    }
+    
+    class Digital{
+        
+    }
+    
+    class Analogico{
+        
+    }
+    
+    class seteSegmentos{
+        seteSegmentos(double x, double y,int tamanho,String cor,int hora, int minuto, int segundo)
+    }
+    
+    class Cronometro{
+        <<interface>>
+    }
+    
+    class Poligonos{
+        
+    }
+    
+    
+    
+```
